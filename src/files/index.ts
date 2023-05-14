a290b28146f4a11eaccd28cc997a942c3860cf28
Stepan Skopivskyi <stepanskopivskyi@macbook-pro-stepan.local>
2023-05-14 03:20:20 +0300

    Fixed local file resource

--- src/files/index.ts
+++ src/files/index.ts
@@ -2,3 +2,4 @@ export * from './content-variables';
 export * from './dotenv';
 export * from './json';
 export * from './yaml';
+export * from './local-file';
--- src/files/local-file/local-file-resource-provider.ts
+++ src/files/local-file/local-file-resource-provider.ts
@@ -1,5 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 
 import { CreateResult, DiffResult, ReadResult, UpdateResult } from '@pulumi/pulumi/dynamic';
 
@@ -13,7 +13,7 @@ export class LocalFileResourceProvider implements pulumi.dynamic.ResourceProvide
      * @param inputs
      */
     async create(inputs: LocalFileProviderInputs): Promise<CreateResult> {
-        await fs.writeFile(process.cwd() + inputs.path, inputs.content);
+        fs.writeFileSync(process.cwd() + inputs.path, inputs.content);
 
         return Promise.resolve({ id: inputs.path, outs: inputs });
     }
@@ -38,7 +38,9 @@ export class LocalFileResourceProvider implements pulumi.dynamic.ResourceProvide
      * @param props
      */
     async delete(id: pulumi.ID, props: LocalFileProviderInputs): Promise<void> {
-        return fs.rm(process.cwd() + props.path);
+        fs.rmSync(process.cwd() + props.path);
+
+        return Promise.resolve(undefined);
     }
 
     /**
@@ -48,7 +50,7 @@ export class LocalFileResourceProvider implements pulumi.dynamic.ResourceProvide
      * @param props
      */
     async read(id: pulumi.ID, props: LocalFileProviderInputs): Promise<ReadResult> {
-        const content = await fs.readFile(process.cwd() + props.path);
+        const content = fs.readFileSync(process.cwd() + props.path).toString();
 
         return Promise.resolve({ id, props: { path: props.path, content } });
     }
@@ -61,7 +63,7 @@ export class LocalFileResourceProvider implements pulumi.dynamic.ResourceProvide
      * @param news
      */
     async update(id: pulumi.ID, olds: LocalFileProviderInputs, news: LocalFileProviderInputs): Promise<UpdateResult> {
-        await fs.writeFile(process.cwd() + news.path, news.content);
+        fs.writeFileSync(process.cwd() + news.path, news.content);
 
         return Promise.resolve({ outs: news });
     }
